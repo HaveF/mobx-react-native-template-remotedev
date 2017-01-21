1b4d6b5b962e46c9425194071563b58c9e0a6655
HaveF <iamaplayer@gmail.com>
2017-01-22 00:42:29 +0800

    unpack rn

---
+++ android/app/src/main/java/com/mobx_react_native_template/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mobx_react_native_template;
+
+import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "mobx_react_native_template";
+    }
+}
